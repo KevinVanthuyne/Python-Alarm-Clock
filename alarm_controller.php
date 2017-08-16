1a0c8e4a288c42d1c6296e07e381c53eec2c3417
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2017-08-16 20:03:50 +0200

    added error message when setting second alarm

--- alarm_controller.php
+++ alarm_controller.php
@@ -23,23 +23,29 @@
         header("Location: index.php?info=" . $info);
     }
     elseif ($action == "Set alarm") {
-        $alarmtime = $_POST["alarmtime"];
-        // if the input is a valid time 00:00 - 23:59
-        if (preg_match("/([01]?[0-9]|2[0-3]):[0-5][0-9]/", $alarmtime)) {
-            $cmd = "python3 /var/www/html/Python-Alarm-Clock/main.py " . $alarmtime;
-
-            // run script, redirect output and error output to log file and run in background(&)
-            echo exec($cmd . " > /var/www/html/Python-Alarm-Clock/script.log 2>&1 &");
-
-            // sleep until alarm file is set
-            while(!file_exists("alarm_set")) {
-                sleep(1);
-            }
-            header("Location: index.php");
+        // if another alarm is already set, give errors
+        if (file_exists("alarm_set")) {
+            header("Location: index.php?info=Another alarm is already set.<br>Cancel the current one first.");
         }
-        // if the input is invalid
         else {
-            header("Location: index.php?info=Invalid input.");
+            $alarmtime = $_POST["alarmtime"];
+            // if the input is a valid time 00:00 - 23:59
+            if (preg_match("/([01]?[0-9]|2[0-3]):[0-5][0-9]/", $alarmtime)) {
+                $cmd = "python3 /var/www/html/Python-Alarm-Clock/main.py " . $alarmtime;
+
+                // run script, redirect output and error output to log file and run in background(&)
+                echo exec($cmd . " > /var/www/html/Python-Alarm-Clock/script.log 2>&1 &");
+
+                // sleep until alarm file is set
+                while(!file_exists("alarm_set")) {
+                    sleep(1);
+                }
+                header("Location: index.php");
+            }
+            // if the input is invalid
+            else {
+                header("Location: index.php?info=Invalid input.");
+            }
         }
     }
 
